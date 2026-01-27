43b3c1ecb1edd0e1da4bf87c95d5f9a6e925a53b
Mason Walker <masonwalker159@gmail.com>
2026-01-27 13:44:11 -0700

    matcha icon

--- docs/blogIcon.js
+++ docs/blogIcon.js
@@ -7,7 +7,7 @@ export async function createBlogIcon(app, world, options = {}) {
 		hoverImageUrl = './assets/spritesheet/hoverMug.png',
 		margin = 24,
 		animationSpeed = 0.12,
-		scale = 3,
+		scale = 20,
 	} = options;
 
 	function extractFrameIndex(name) {
