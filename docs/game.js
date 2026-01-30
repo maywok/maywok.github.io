7877f260c1f94e8ee6b017398f90b598058cb9db
Mason Walker <masonwalker159@gmail.com>
2026-01-29 20:07:06 -0700

    change fisheye value

--- docs/game.js
+++ docs/game.js
@@ -99,7 +99,7 @@ async function boot() {
 				intensity: 0.08,
 				brightness: 0.06,
 				scanStrength: 0.85,
-				curve: 0.04,
+				curve: 0.03,
 				vignette: 0.28,
 			});
 			const { filter: crtScanlinesFilter, uniforms: crtScanlinesUniforms } = createCRTScanlinesFilter(app, {
